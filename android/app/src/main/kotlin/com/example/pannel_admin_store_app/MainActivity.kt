aae4aedfac24be41193da48117cd424c0c1015ff
Daniyal <daniyaldehghan2021@gmail.com>
2024-09-03 03:26:09 +0330

    first commit

---
+++ android/app/src/main/kotlin/com/example/pannel_admin_store_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pannel_admin_store_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/views/main_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_admin_scaffold/admin_scaffold.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const AdminScaffold(
+      body: Text('Dashboard'),
+    );
+  }
+}
