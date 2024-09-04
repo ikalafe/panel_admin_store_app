f5044aa78571241d745218ffe53449aeb5aa9794
Daniyal <daniyaldehghan2021@gmail.com>
2024-09-05 01:17:08 +0330

    Create a model for the category - Create model folder and category.dart file for categories -Creating the Category class and defining the fields required by the category

---
+++ lib/views/models/category.dart
@@ -0,0 +1,13 @@
+class Category {
+  final String id;
+  final String name;
+  final String image;
+  final String banner;
+
+  Category({
+    required this.id,
+    required this.name,
+    required this.image,
+    required this.banner,
+  });
+}
