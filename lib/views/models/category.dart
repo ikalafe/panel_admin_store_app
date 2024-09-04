cc912494f4096ce8cd74128a261fd22f24f92725
Daniyal <daniyaldehghan2021@gmail.com>
2024-09-05 01:42:05 +0330

    Add serialization methods to Category model
    - Implemented toMap and fromMap methods for converting Category objects to and from maps.
    - Added toJson and fromJson methods for serializing and deserializing Category objects as JSON.

--- lib/views/models/category.dart
+++ lib/views/models/category.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 class Category {
   final String id;
   final String name;
@@ -10,4 +13,27 @@ class Category {
     required this.image,
     required this.banner,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'image': image,
+      'banner': banner
+    };
+  }
+
+  factory Category.fromMap(Map<String, dynamic> map) {
+    return Category(
+      id: map['id'] as String,
+      name: map['name'] as String,
+      image: map['image'] as String,
+      banner: map['banner'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Category.fromJson(String source) =>
+      Category.fromMap(json.decode(source) as Map<String, dynamic>);
 }
