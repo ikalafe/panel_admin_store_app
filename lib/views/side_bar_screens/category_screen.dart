2ef1d82c239e7cecb8b111f3cbdb8ed84ef7de5e
Daniyal <daniyaldehghan2021@gmail.com>
2024-09-04 21:37:41 +0330

    Checking and validating user input for the category name

--- lib/views/side_bar_screens/category_screen.dart
+++ lib/views/side_bar_screens/category_screen.dart
@@ -11,6 +11,8 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late String categoryName;
   dynamic _image;
   pickImage() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles(
@@ -26,164 +28,179 @@ class _CategoryScreenState extends State<CategoryScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(right: 20.0, top: 17),
-          child: Container(
-            alignment: Alignment.topRight,
-            child: const Text(
-              'دسته بندی ها',
-              style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0, top: 17),
+            child: Container(
+              alignment: Alignment.topRight,
+              child: const Text(
+                'دسته بندی ها',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
-        ),
-        Divider(
-          color: Colors.grey.shade400,
-        ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0, right: 20),
-              child: Container(
-                width: 170,
-                height: 170,
-                decoration: BoxDecoration(
-                  color: Colors.black38,
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                alignment: Alignment.center,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(16),
-                  child: _image != null
-                      ? Image.memory(
-                          _image,
-                          fit: BoxFit.cover,
-                          width: 170,
-                          height: 170,
-                        )
-                      : const Text(
-                          'تصویر دسته بندی',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500),
-                        ),
+          Divider(
+            color: Colors.grey.shade400,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, right: 20),
+                child: Container(
+                  width: 170,
+                  height: 170,
+                  decoration: BoxDecoration(
+                    color: Colors.black38,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  alignment: Alignment.center,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: _image != null
+                        ? Image.memory(
+                            _image,
+                            fit: BoxFit.cover,
+                            width: 170,
+                            height: 170,
+                          )
+                        : const Text(
+                            'تصویر دسته بندی',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500),
+                          ),
+                  ),
                 ),
               ),
-            ),
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0, right: 20),
-                  child: SizedBox(
-                    width: 230,
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'نام دسته بندی',
-                        floatingLabelStyle:
-                            const TextStyle(color: Colors.black87),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: Color(0xff60A5FA),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0, right: 20),
+                    child: SizedBox(
+                      width: 230,
+                      child: TextFormField(
+                        onChanged: (value) => categoryName = value,
+                        validator: (value) {
+                          if (value!.isNotEmpty) {
+                            return null;
+                          } else {
+                            return 'لطفا نام دسته بندی را وارد کنید';
+                          }
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'نام دسته بندی',
+                          floatingLabelStyle:
+                              const TextStyle(color: Colors.black87),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0xff60A5FA),
+                            ),
+                            borderRadius: BorderRadius.circular(16),
                           ),
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 20, top: 15),
-                  child: SizedBox(
-                    width: 230,
-                    height: 50,
-                    child: ElevatedButton.icon(
-                      label: const Text(
-                        "انتخاب و بارگزاری تصویر",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xff5796E4),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12))),
-                      onPressed: () {
-                        pickImage();
-                      },
-                      icon: const Icon(
-                        CupertinoIcons.cloud_upload,
-                        color: Colors.white,
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20, top: 15),
+                    child: SizedBox(
+                      width: 230,
+                      height: 50,
+                      child: ElevatedButton.icon(
+                        label: const Text(
+                          "انتخاب و بارگزاری تصویر",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xff5796E4),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12))),
+                        onPressed: () {
+                          pickImage();
+                        },
+                        icon: const Icon(
+                          CupertinoIcons.cloud_upload,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.only(right: 20),
-                      width: 120,
-                      height: 40,
-                      child: TextButton(
-                        style: ButtonStyle(
-                          backgroundColor: WidgetStateProperty.all<Color>(
-                              Colors.transparent),
-                          shape:
-                              WidgetStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                              side: const BorderSide(
-                                color: Color(0xff5796E4),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(right: 20),
+                        width: 120,
+                        height: 40,
+                        child: TextButton(
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStateProperty.all<Color>(
+                                Colors.transparent),
+                            shape:
+                                WidgetStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                side: const BorderSide(
+                                  color: Color(0xff5796E4),
+                                ),
                               ),
                             ),
                           ),
+                          onPressed: () {},
+                          child: const Text("لغو بارگزاری"),
                         ),
-                        onPressed: () {},
-                        child: const Text("لغو بارگزاری"),
                       ),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    SizedBox(
-                      width: 120,
-                      height: 40,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.green,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        width: 120,
+                        height: 40,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              debugPrint(categoryName);
+                            }
+                          },
+                          child: const Text(
+                            "ذخیره",
+                            style: TextStyle(color: Colors.white),
                           ),
                         ),
-                        onPressed: () {},
-                        child: const Text(
-                          "ذخیره",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-              ],
-            )
-          ],
-        ),
-        Divider(
-          color: Colors.grey.shade400,
-        ),
-      ],
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              )
+            ],
+          ),
+          Divider(
+            color: Colors.grey.shade400,
+          ),
+        ],
+      ),
     );
   }
 }
